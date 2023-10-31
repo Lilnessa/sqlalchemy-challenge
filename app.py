ecdb21066193d076f09e8b8f37cfcb5fd2666666
Lilnessa <140993456+Lilnessa@users.noreply.github.com>
2023-10-30 23:39:16 -0700

    sync r2

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ Base = automap_base()
 
 # reflect the tables
 Base.prepare(autoload_with=engine)
-Base.classes.keys()
+# Base.classes.keys()
 
 # Save references to each table
 Measurements = Base.classes.measurement
@@ -31,6 +31,15 @@ Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
+recent_dateset = session.query(Measurements.date).order_by(Measurements.date.desc()).first()
+recent_date = recent_dateset.date
+prior_one_year = dt.datetime.strptime(recent_date,'%Y-%m-%d') - dt.timedelta(days=366)
+active_stations = session.query(Measurements.station,func.count(Measurements.station)).group_by(Measurements.station).\
+    order_by(func.count(Measurements.station).desc()).all()
+most_active = active_stations[0][0]
+active_caluc = session.query(func.min(Measurements.tobs), func.max(Measurements.tobs), func.avg(Measurements.tobs)).\
+    filter(Measurements.station == most_active).all()
+session.close()
 
 #################################################
 # Flask Setup
@@ -52,13 +61,10 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    recent_dateset = session.query(Measurements.date).order_by(Measurements.date.desc()).first()
-    recent_date = recent_dateset.date
-    prior_one_year = dt.datetime.strptime(recent_date,'%Y-%m-%d') - dt.timedelta(days=366)
     precipitation_scores = session.query(Measurements.date,Measurements.prcp).filter(Measurements.date >= prior_one_year).all()
 
-    precipitation_list = []
-    for date, prcp in results:
+    prcp_list = []
+    for date, prcp in prcp_list:
         prcp_dict = {}
         prcp_dict["date"] = date
         prcp_dict["prcp"] = prcp
@@ -77,9 +83,9 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def temperature():
     temperature_active = session.query(Measurements.date, Measurements.tobs).\
-    filter(Measurements.station == most_active).\
-    filter(Measurements.date >= prior_one_year).\
-    group_by(Measurements.date).all()
+        filter(Measurements.station == most_active).\
+        filter(Measurements.date >= prior_one_year).\
+        group_by(Measurements.date).all()
     
     return jsonify(temperature)
 
@@ -95,9 +101,6 @@ def start_end():
     startend_date = session.query(func.min(Measurements.tobs),func.avg(Measurements.tobs),func.max(Measurements.tobs)).\
         filter(Measurements.date >= prior_one_year).all()
     return jsonify(startend_date)
-
-
-session.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
