8ab4f516b08c61a21772758f926e7262de161ea5
Lilnessa <140993456+Lilnessa@users.noreply.github.com>
2023-10-31 23:50:23 -0700

    Sync r3

--- app.py
+++ app.py
@@ -23,23 +23,18 @@ Base = automap_base()
 
 # reflect the tables
 Base.prepare(autoload_with=engine)
-# Base.classes.keys()
+Base.classes.keys()
 
 # Save references to each table
-Measurements = Base.classes.measurement
+Measurement = Base.classes.measurement
 Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
-recent_dateset = session.query(Measurements.date).order_by(Measurements.date.desc()).first()
+
+recent_dateset = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
 recent_date = recent_dateset.date
 prior_one_year = dt.datetime.strptime(recent_date,'%Y-%m-%d') - dt.timedelta(days=366)
-active_stations = session.query(Measurements.station,func.count(Measurements.station)).group_by(Measurements.station).\
-    order_by(func.count(Measurements.station).desc()).all()
-most_active = active_stations[0][0]
-active_caluc = session.query(func.min(Measurements.tobs), func.max(Measurements.tobs), func.avg(Measurements.tobs)).\
-    filter(Measurements.station == most_active).all()
-session.close()
 
 #################################################
 # Flask Setup
@@ -61,46 +56,52 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    precipitation_scores = session.query(Measurements.date,Measurements.prcp).filter(Measurements.date >= prior_one_year).all()
+    prcp_scores = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date >= prior_one_year).all()
 
     prcp_list = []
-    for date, prcp in prcp_list:
+    for date, prcp in prcp_scores:
         prcp_dict = {}
         prcp_dict["date"] = date
         prcp_dict["prcp"] = prcp
-        prcp_list.append(prcp_dict)
+        prcp_scores.append(prcp_dict)
 
     return jsonify(prcp_list)
     
 
 @app.route("/api/v1.0/stations")
 def stations():
-    stations_list = session.query(Measurements.station).group_by(Measurements.station).\
-    order_by(func.count(Measurements.station).desc()).all()
+    stations_list = session.query(Measurement.station).group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).all()
     
     return jsonify(stations_list)
 
 @app.route("/api/v1.0/tobs")
 def temperature():
-    temperature_active = session.query(Measurements.date, Measurements.tobs).\
-        filter(Measurements.station == most_active).\
-        filter(Measurements.date >= prior_one_year).\
-        group_by(Measurements.date).all()
+    active_stations = session.query(Measurement.station,func.count(Measurement.station)).group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).all()
+    most_active = active_stations[0][0]
+    
+    temperature = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == most_active).\
+        filter(Measurement.date >= prior_one_year).\
+        group_by(Measurement.date).all()
     
     return jsonify(temperature)
 
 @app.route("/api/v1.0/<start>")
 def start():
-    start_date = session.query(func.min(Measurements.tobs),func.avg(Measurements.tobs),func.max(Measurements.tobs)).\
-        filter(Measurements.date == prior_one_year).all()
+    start_date = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+        filter(Measurement.date == prior_one_year).all()
     
     return jsonify(start_date)
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end():
-    startend_date = session.query(func.min(Measurements.tobs),func.avg(Measurements.tobs),func.max(Measurements.tobs)).\
-        filter(Measurements.date >= prior_one_year).all()
+    startend_date = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+        filter(Measurement.date >= prior_one_year).all()
     return jsonify(startend_date)
+
+session.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
